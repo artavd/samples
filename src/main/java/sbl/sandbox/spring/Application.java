1b8c02d01869934fcd311e8c9ab5d21bac7fcc6b
Artem Avdosev <artem.avdosev@gmail.com>
2016-02-15 23:03:55 +0300

    Spring Scheduling sandbox reworked to be a common Spring sandbox

---
+++ src/main/java/sbl/sandbox/spring/Application.java
@@ -0,0 +1,14 @@
+package sbl.sandbox.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+    public static void main(String[] argvs) throws Exception {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/sbl/sandbox/spring/scheduling/ScheduledTasks.java
@@ -0,0 +1,17 @@
+package sbl.sandbox.spring.scheduling;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println(String.format("The time is now %s", dateFormat.format(new Date())));
+    }
+}
